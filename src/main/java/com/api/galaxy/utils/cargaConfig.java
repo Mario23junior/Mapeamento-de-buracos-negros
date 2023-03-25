b0c046ffe231af163a73b9a9e4f15525b1e21d8d
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-03-25 01:40:17 -0300

    Algoritmo probabilístico  busca de buracos negros

---
+++ src/main/java/com/api/galaxy/utils/cargaConfig.java
@@ -0,0 +1,30 @@
+package com.api.galaxy.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.api.galaxy.cargaBase.cargaBlackHoleData;
+import com.api.galaxy.cargaBase.cargaService;
+
+@Configuration
+@Profile("dev")
+public class cargaConfig {
+
+	@Autowired
+	private cargaService powerDataBase;
+
+	@Autowired
+	private cargaBlackHoleData cargaBlackHole;
+
+	@Bean
+	public void cargaBaseData() {
+		this.powerDataBase.powerDataBase();
+	}
+
+	@Bean
+	public void powerDataBlackHoleCarga() {
+		this.cargaBlackHole.powerDataBlackHoleCarga();
+	}
+}
