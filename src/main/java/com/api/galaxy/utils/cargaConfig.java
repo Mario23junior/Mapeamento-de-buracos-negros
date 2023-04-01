504e61e0600550e2faef11e1ac36b5589d1805fc
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-04-01 00:37:16 -0300

    Removendo cargas de galaxias não utilizadas

--- src/main/java/com/api/galaxy/utils/cargaConfig.java
+++ src/main/java/com/api/galaxy/utils/cargaConfig.java
@@ -6,23 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.api.galaxy.cargaBase.cargaBlackHoleData;
-import com.api.galaxy.cargaBase.cargaService;
 
 @Configuration
 @Profile("dev")
 public class cargaConfig {
 
-	@Autowired
-	private cargaService powerDataBase;
 
 	@Autowired
 	private cargaBlackHoleData cargaBlackHole;
 
-	@Bean
-	public void cargaBaseData() {
-		this.powerDataBase.powerDataBase();
-	}
-
+ 
 	@Bean
 	public void powerDataBlackHoleCarga() {
 		this.cargaBlackHole.powerDataBlackHoleCarga();
