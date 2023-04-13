e40e96f97474b9f877b01b9b42a7505f2b33286e
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-04-13 13:24:49 -0300

    Inserindo repository de busca de desvio ao vermelho

--- src/main/java/com/api/galaxy/repository/BlackHoleRepositpry.java
+++ src/main/java/com/api/galaxy/repository/BlackHoleRepositpry.java
@@ -1,9 +1,12 @@
 package com.api.galaxy.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.galaxy.model.BlackHole;
 
 public interface BlackHoleRepositpry extends JpaRepository<BlackHole, Long>{
 	
+	Optional<BlackHole> findByRedshift(Double predic);
 }
